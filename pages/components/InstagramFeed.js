6ce707e256855ff0c2915f1c7d79d1b495d8315d
Jeremy Smith <prez@Jeremys-MacBook-Air.local>
2023-12-29 21:01:26 -0800

    wip

--- pages/components/InstagramFeed.js
+++ pages/components/InstagramFeed.js
@@ -1,18 +1,18 @@
-import React from 'react';
+// import React from 'react';
 
-const InstagramGallery = ({ posts }) => {
-  return (
-    <div>
-      <h1>My Instagram Gallery</h1>
-      <div>
-        {posts.data.map(post => (
-          <div key={post.id}>
-            <img src={post.media_url} alt={post.caption} />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
+// const InstagramGallery = ({ posts }) => {
+//   return (
+//     <div>
+//       <h1>My Instagram Gallery</h1>
+//       <div>
+//         {posts.data.map(post => (
+//           <div key={post.id}>
+//             <img src={post.media_url} alt={post.caption} />
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
 
-export default InstagramGallery;
+// export default InstagramGallery;
